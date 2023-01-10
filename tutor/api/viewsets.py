2791c0e82f772bcc38ff22636fa517acd03c3072
Noe Vazquez <famvazpom@gmail.com>
2023-01-09 23:24:20 -0500

    Eliminar autenticacion

--- tutor/api/viewsets.py
+++ tutor/api/viewsets.py
@@ -6,19 +6,19 @@ from .serializers import *
 class MateriaViewSet(viewsets.ModelViewSet):
     queryset = Materia.objects.all()
     serializer_class = MateriaSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = []
 
 class TemaViewSet(viewsets.ModelViewSet):
     queryset = Tema.objects.all()
     serializer_class = TemaSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = []
 
 class ExplicacionViewSet(viewsets.ModelViewSet):
     queryset = Explicacion.objects.all()
     serializer_class = ExplicacionSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = []
 
 class EjercicioViewSet(viewsets.ModelViewSet):
     queryset = Ejercicio.objects.all()
     serializer_class = EjercicioSerializer
-    permission_classes = [IsAuthenticated]
+    permission_classes = []
